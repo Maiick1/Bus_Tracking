860e2b4ef4401a2494b077b423872c39d37206d7
Mike <msvillacres@itb.edu.ec>
2025-10-07 10:57:47 -0500

    Delete Android directory

--- Android/app/main/java/kotlin/com/busticket_pro/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.busticket_pro.app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterFragmentActivity() {
-}
